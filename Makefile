ae5162829e88cffaed588fbed86da05d8a8a4801
MM <mmayer@mayeranalytics.com>
2018-06-11 08:44:03 +0200

    add RunAvgVector

--- Makefile
+++ Makefile
@@ -6,7 +6,8 @@ build:
 run: build
 	stack exec -- run-repeat && \
 	stack exec -- run-avg && \
-	stack exec -- run-avg-streaming
+	stack exec -- run-avg-streaming && \
+	stack exec -- run-avg-vector
 
 build-profile:
 	stack build --profile
@@ -15,6 +16,7 @@ profile: build-profile
 	stack exec -- run-repeat +RTS -p -RTS; \
 	stack exec -- run-avg +RTS -p -RTS; \
 	stack exec -- run-avg-streaming +RTS -p -RTS
+	stack exec -- run-avg-vector +RTS -p -RTS
 
 code:
 	stack build stylish-haskell hlint intero hoogle hsc2hs && \
---
+++ RunAvgVector.hs
@@ -0,0 +1,34 @@
+import qualified Data.Vector.Fusion.Stream.Monadic as V
+import           LFSR
+import           System.TimeIt                     (timeItT)
+
+step1 :: LFSR -> IO Word32
+step1 lfsr = stepLFSR lfsr >> getLFSR lfsr
+
+step1' :: LFSR -> IO Double
+step1' lfsr = fromIntegral <$> step1 lfsr
+
+avg :: LFSR -> Int -> IO Double
+avg lfsr n = do
+    let stream = V.replicateM n (step1' lfsr)
+    V.foldl (+) 0.0 stream
+
+main :: IO ()
+main = do
+    let nIter = 10000000    -- number of iterations
+
+    lfsr <- newLFSR
+
+    putStrLn "=== RunVector ========="
+
+    (tBaseLine, _) <- timeItT $ do
+        setLFSR lfsr 42
+        avgLFSR lfsr nIter
+    putStrLn $ "Baseline: " ++ show tBaseLine
+
+    (t, _) <- timeItT $ do
+        setLFSR lfsr 42
+        avg lfsr nIter
+    putStrLn $ "IO:       " ++ show t
+
+    putStrLn $ "factor:   " ++ show (t/tBaseLine)
