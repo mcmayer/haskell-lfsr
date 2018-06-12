df1193be6ea090edc7200278d835ef5c3a941f2f
MM <mmayer@mayeranalytics.com>
2018-06-12 05:31:59 +0200

    add some doc

--- lfsr/lfsr.h
+++ lfsr/lfsr.h
@@ -1,18 +1,22 @@
 #include <stdint.h>
 
 typedef struct {
-    uint32_t state;
+    uint32_t state;     // carries the LFSR state
  } state_t;
 
+/* constructor */
 state_t* 
 new_lfsr();
 
+/* destructor */
 void 
 delete_lfsr(state_t*);
 
+/* setter */
 void 
 set_lfsr(state_t* s, uint32_t value);
 
+/* getter */
 uint32_t
 get_lfsr(state_t* s);
 
@@ -24,5 +28,6 @@ step_lfsr(state_t* s);
 void
 repeat_lfsr(state_t* s, int n);
 
+/* calculate average over n iterations */
 double 
 avg(state_t* s, int n);
